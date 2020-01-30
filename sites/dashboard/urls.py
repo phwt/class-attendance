e66d7b59ca9670a2d81e88ea12c8f5811c897c9c
phwt <phwt.smwt@gmail.com>
2020-01-30 16:45:26 +0700

    Update dashboard

--- sites/dashboard/urls.py
+++ sites/dashboard/urls.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from dashboard import views
 urlpatterns = [
+    path('student_list/', views.student_list),
+    path('student_add/', views.student_add),
+    path('student_edit/<int:sid>', views.student_edit),
+    path('course_list/', views.course_list),
+    path('course_add/', views.course_add),
+    path('course_edit/<int:cid>', views.course_edit)
 ]
--- sites/dashboard/views.py
+++ sites/dashboard/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def student_list(request):
+    return HttpResponse('student_list')
+
+def student_add(request):
+    return HttpResponse('student_add')
+
+def student_edit(request, sid):
+    return HttpResponse(f'student_edit: {sid}')
+
+def course_list(request):
+    return HttpResponse('course_list')
+
+def course_add(request):
+    return HttpResponse('course_add')
+
+def course_edit(request, cid):
+    return HttpResponse(f'course_edit: {cid}')
+
