72ff2bc9ffd5442e9cf11a9e9d271f1d1e1a09c4
phwt <phwt.smwt@gmail.com>
2020-01-30 16:40:04 +0700

    Update attendance

--- sites/attendance/urls.py
+++ sites/attendance/urls.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from attendance import views
 urlpatterns = [
+    path('subject/', views.subject),
+    path('course/<int:id>', views.course),
+    path('qrcode/', views.qrcode)
 ]
--- sites/attendance/views.py
+++ sites/attendance/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def subject(request):
+    return HttpResponse('Show today subject')
+
+def course(request, id):
+    return HttpResponse('วิชานี้สอนอะไร, มีจำนวนนักเรียนกี่คน, มีคนมาเรียน และขาดกี่คน')
+
+def qrcode(request):
+    return HttpResponse('QR Code')
